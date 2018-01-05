fd3164b867bb8768b90ba53cab42ab024c16d87a
Canario0 <prenero4bc@gmail.com>
2018-01-05 18:43:42 +0100

    Bug fixed
    there was a bug as consecuence of computing delay

--- Python+Arduino/arduino/src/Arduino.ino
+++ Python+Arduino/arduino/src/Arduino.ino
@@ -1,62 +1,74 @@
 
-String data="";
-boolean is_complete=false;
-boolean ack = true;
-boolean up =false;//to check if there is someoen reading
+String data = "";
+boolean is_complete = false;
+boolean ack = false;
+boolean up = false; //to check if there is someoen reading
 
-void setup() {
+void setup()
+{
     //Initialize serial and wait for port to open:
     Serial.begin(9600); //baud rate for arduino 1
-    while (!Serial) {
-      ; // wait for serial port to connect. Needed for native USB port only
+    while (!Serial)
+    {
+        ; // wait for serial port to connect. Needed for native USB port only
     }
     // prints title with ending line break
     Serial.println("Conneted, waiting for Hand Sake\n");
-    data.reserve(3);// reserve  3 bytes for the string
-    }
+    data.reserve(3); // reserve  3 bytes for the string
+}
 
-
-void loop(){
-    if(up){
-        //Prints what he recived
-        if(is_complete){
-           //Print the string
-           Serial.print("I recived: ");
-           Serial.println(data);
-           //Clear the string
-           data="";
-           is_complete=false;
-        }
-        
+void loop()
+{
+    if (up)
+    {
         //Print some data, and wait to ACk to print again
-        if(ack){
+        if (ack)
+        {
             Serial.print("Hey, are u up.");
             Serial.println(" Ok, I leave u this.");
+            delay(1000);
             ack = false;
         }
     }
+    //Prints what he recived
+    if (is_complete)
+    {
+        //Print the string
+        Serial.print("I recived: ");
+        Serial.println(data);
+        //Clear the string
+        data = "";
+        is_complete = false;
+    }
 }
 
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    if (inChar == '\n') {
-      is_complete = true;
-    }else{
-        data += inChar;
+void serialEvent()
+{
+    while (Serial.available())
+    {
+        // get the new byte:
+        char inChar = (char)Serial.read();
+        // add it to the inputString:
+        if (inChar == '\n')
+        {
+            is_complete = true;
+        }
+        else
+        {
+            data += inChar;
+        }
+        // if the incoming character is a newline, set a flag so the main loop can
+        // do something about it:
     }
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-  }
 
-  if(data=="ACK"){
-      ack=true;
-  }else if(data=="UP "){
-      up=true;
-  }else if(data=="EXI"){
-      up=false;
-      Serial.println("Bye");
-  } 
+    if (data == "UP ")
+    {
+        ack = true;
+        up = true;
+    }
+    else if (data == "EXI")
+    {
+        up = false;
+        Serial.println("Bye");
+    }
 }
--- Python+Arduino/python/src/Prueba.py
+++ Python+Arduino/python/src/Prueba.py
@@ -1,11 +1,14 @@
 import serial
+from time import sleep
 
 
 ser = serial.Serial('/dev/ttyACM0',9600)  # open serial port
 print(ser.name) # print serial port name
 ser.write(b"UP \n") # write to arduino that is UP to read
-data=ser.read(35) # read
+data=ser.read(33) # read
 print(data)      # print the data
 ser.write(b"ACK\n") # write ACK
+sleep(1)
 ser.write(b"EXI\n") # close connection
+ser.flush()
 ser.close()             # close port
